dfa6e7b8471f39b94f950597cc947e478b1b3705
Brad White <bradscottwhite@gmail.com>
2023-09-20 21:01:19 -0400

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO beer(name,abv) VALUES('Kozel', 9.0);
+INSERT INTO beer(name,abv) VALUES('Carlsberg', 9.0);
+INSERT INTO beer(name,abv) VALUES('Heineken', 7.5);
+INSERT INTO beer(name,abv) VALUES('Tuborg', 5.0);
+INSERT INTO beer(name,abv) VALUES('Bochka', 6.2);
+COMMIT;
